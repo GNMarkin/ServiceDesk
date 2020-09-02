aa7c431104e0141ed7c6b936cee65a5c98270080
Gennady Markin <gnmarkin@gmail.com>
2020-09-02 11:34:10 +0300

    Подключение таблицы в Заявках

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -1,7 +1,8 @@
 "use strict";
 
 //получить список заявок
-$.post("/getOrders", {}, ordersList => refreshOrders(ordersList));
+// $.post("/getOrders", {}, ordersList => refreshOrders(ordersList));
+refreshOrders();
 
 //добавить новую заявку
 $(".addNewOrder").on("click", function () {
@@ -10,27 +11,63 @@ $(".addNewOrder").on("click", function () {
     //отправить новую заявку по маршруту
     $.post("/addNewOrder", { newOrder }, function (ordersList) {
         //TODO: 200630-1043 Добавить обработчик ошибок при добавлении новой заявки
-        refreshOrders(ordersList);
+        refreshOrders();
         socket.send(newOrder);
     });
 });
 
 //обновить список заявок
-function refreshOrders(ordersList) {
-    let $ordersList = $(".ordersList");
-    $ordersList.empty();
-    ordersList.forEach(order => $ordersList.append($("<li>").text(order.description)));
+function refreshOrders() {
+    $('#table_id').DataTable({
+        ajax: {
+            url: '/getOrders',
+            type: 'POST',
+            dataSrc: ''
+        },
+        columns: [
+            { data: 'index' },
+            { data: 'date' },
+            { data: 'description' },
+            { data: 'customer' },
+            { data: 'worker' },
+            { data: 'status' }
+        ]
+    });
+
+    //     $('#table_id').DataTable({
+    //         data: ordersList,
+    //         columns: [
+    //             { data: 'index' },
+    //             // { data: 'date' },
+    //             { data: 'date',
+    //                 render: function (data, type, row) {
+    //                     // let dateSplit = data.split('-');
+    //                     let dateSplit = data.replace(/(\d+)-(\d+)-(\d+)/, '$3/$2/$1');
+    //                     dateSplit = dateSplit.replace(RegExp("/","g"), ".");
+    //                     dateSplit = dateSplit.replace("T", " ");
+    //                     return type === "display" || type === "filter" ? dateSplit : data;
+    //                 }
+    //             },
+    //             { data: 'description' },
+    //             { data: 'customer' },
+    //             { data: 'worker' },
+    //             { data: 'status' }
+    //         ]
+    //     });
 };
 
 // проверяем поддержку WebSocket
-if (!window.WebSocket) {console.log('WebSocket в этом браузере не поддерживается.');};
+if (!window.WebSocket) {
+    console.log('WebSocket в этом браузере не поддерживается.');
+} else {
+    // создаем подключение
+    //TODO: получить текущий ip адрес сервера
+    let socket = new WebSocket("ws://192.168.0.13:8081");
 
-// создаем подключение
-//TODO: получить текущий ip адрес сервера
-let socket = new WebSocket("ws://192.168.0.13:8081");
-
-// слушаем входящие сообщения и обрабатываем их
-socket.onmessage = function (event) {
-    //let incomingMessage = event.data;
-    $.post("/getOrders", {}, ordersList => refreshOrders(ordersList));
+    // слушаем входящие сообщения и обрабатываем их
+    socket.onmessage = function (event) {
+        //let incomingMessage = event.data;
+        //$.post("/getOrders", {}, ordersList => refreshOrders(ordersList));
+        refreshOrders();
+    };
 };
